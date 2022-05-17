72012fe1f2cc9d391a8d095cc6b42e7505c1e179
barsimark <barsimark@gmail.com>
2022-05-17 19:31:38 +0200

    jacoco init

--- src/test/java/application/SortTest.java
+++ src/test/java/application/SortTest.java
@@ -9,13 +9,14 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import org.junit.jupiter.api.BeforeEach;
 
 public class SortTest {
-    int[] arrayShort, arrayLong, arrayNegative;
+    int[] arrayShort, arrayLong, arrayNegative, arrayEmpty;
 
     @BeforeEach
     public void init(){
         arrayShort = new int[]{5, 8, 2, 6, 11};
         arrayLong = new int[]{5, 8, 2, 6, 11, 215, 645, 312, 2, 4, 1154, 44, 69, 420};
         arrayNegative = new int[]{0, -4, -10, 5, -35, -4, -2, -1};
+        arrayEmpty = new int[0];
     }
 
     @Test
@@ -23,9 +24,11 @@ public class SortTest {
         BubbleSort.bubbleSort(arrayShort);
         BubbleSort.bubbleSort(arrayLong);
         BubbleSort.bubbleSort(arrayNegative);
+        BubbleSort.bubbleSort(arrayEmpty);
 
         assertArrayEquals(new int[]{2, 5, 6, 8, 11}, arrayShort);
         assertArrayEquals(new int[]{2, 2, 4, 5, 6, 8, 11, 44, 69, 215, 312, 420, 645, 1154}, arrayLong);
         assertArrayEquals(new int[]{-35, -10, -4, -4, -2, -1, 0, 5}, arrayNegative);
+        assertArrayEquals(new int[]{}, arrayEmpty);
     }
 }
