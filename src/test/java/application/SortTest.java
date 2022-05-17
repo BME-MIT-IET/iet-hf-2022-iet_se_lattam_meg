a8283c961ead5cbc9aade9efbf5b87f44a1ad1e3
barsimark <barsimark@gmail.com>
2022-05-17 14:43:26 +0200

    basic unit test for Bubble Sort

---
+++ src/test/java/application/SortTest.java
@@ -0,0 +1,31 @@
+package application;
+
+import org.junit.jupiter.api.Test;
+
+import application.bubble_sort.BubbleSort;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class SortTest {
+    int[] arrayShort, arrayLong, arrayNegative;
+
+    @BeforeEach
+    public void init(){
+        arrayShort = new int[]{5, 8, 2, 6, 11};
+        arrayLong = new int[]{5, 8, 2, 6, 11, 215, 645, 312, 2, 4, 1154, 44, 69, 420};
+        arrayNegative = new int[]{0, -4, -10, 5, -35, -4, -2, -1};
+    }
+
+    @Test
+    public void Test_Bubble_Sort(){
+        BubbleSort.bubbleSort(arrayShort);
+        BubbleSort.bubbleSort(arrayLong);
+        BubbleSort.bubbleSort(arrayNegative);
+
+        assertArrayEquals(new int[]{2, 5, 6, 8, 11}, arrayShort);
+        assertArrayEquals(new int[]{2, 2, 4, 5, 6, 8, 11, 44, 69, 215, 312, 420, 645, 1154}, arrayLong);
+        assertArrayEquals(new int[]{-35, -10, -4, -4, -2, -1, 0, 5}, arrayNegative);
+    }
+}
