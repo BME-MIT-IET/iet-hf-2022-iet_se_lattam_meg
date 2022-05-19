c3f0dd6680902ecab7b47d1d8a40d467ffaad6ca
martonistvan0330 <mistvan0330@gmail.com>
2022-05-19 19:21:50 +0200

    fixed code duplication

--- src/main/java/PerformanceTestMain.java
+++ src/main/java/PerformanceTestMain.java
@@ -1,3 +1,5 @@
+import application.bead_sort.BeadSort;
+import application.bubble_sort.BubbleSort;
 import application.circle_sort.CircleSort;
 import application.cocktail_sort.CocktailSort;
 import application.cycle_sort.CycleSort;
@@ -13,33 +15,34 @@ import application.shell_sort.ShellSort;
 
 import java.util.*;
 
-import static application.bead_sort.BeadSort.beadSort;
-import static application.bubble_sort.BubbleSort.bubbleSort;
 
 public class PerformanceTestMain {
-    private static Random random = new Random();
-
+    private static final Random random = new Random();
     private static ArrayList<int[]> inputs;
     private static final HashMap<String, Long> times = new HashMap<>();
     private static final HashMap<String, Long> memories = new HashMap<>();
     private static int tabCount = 0;
 
+    interface SortingAlgorithm {
+        void sort(int[] input);
+    }
+
     public static void main(String[] args) {
         inputs = createInputs(10000, 1000);
-        beadSortTest();
-        bubbleSortTest();
-        circleSortTest();
-        cocktailSortTest();
-        cycleSortTest();
-        gnomeSortTest();
-        heapSortTest();
-        insertionSortTest();
-        pigeonHoleSortTest();
-        quickSortTest();
-        radixSortTest();
-        selectionSortTest();
-        shakerSortTest();
-        shellSortTest();
+        testSortingAlgorithm(BeadSort::beadSort, "Bead");
+        testSortingAlgorithm(BubbleSort::bubbleSort, "Bubble");
+        testSortingAlgorithm(CircleSort::Sort, "Circle");
+        testSortingAlgorithm(input -> new CocktailSort().sort(input), "Cocktail");
+        testSortingAlgorithm(input -> CycleSort.cycleSort(input, input.length), "Cycle");
+        testSortingAlgorithm(GnomeSort::gnomeSort, "Gnome");
+        testSortingAlgorithm(HeapSort::sort, "Heap");
+        testSortingAlgorithm(input -> new InsertionSort().sort(input), "Insertion");
+        testSortingAlgorithm(input -> PigeonHoleSort.pigeonholeSort(input, input.length), "Pigeon hole");
+        testSortingAlgorithm(input -> new QuickSort().sort(input, 0, input.length - 1), "Quick");
+        testSortingAlgorithm(RadixSort::radixSort, "Radix");
+        testSortingAlgorithm(SelectionSort::sort, "Selection");
+        testSortingAlgorithm(ShakerSort::sort, "Shaker");
+        testSortingAlgorithm(ShellSort::sort, "Shell");
         printTopSpeed();
         printTopMemory();
     }
@@ -63,7 +66,7 @@ public class PerformanceTestMain {
         for (int i = 1; i <= 5; i++) {
             System.out.print(i);
             System.out.print(". ");
-            System.out.print(sorted.get(i-1).getKey());
+            System.out.print(sorted.get(i-1).getKey() + " sort");
             System.out.print(" (");
             System.out.print(sorted.get(i-1).getValue());
             System.out.println(" MB)");
@@ -76,359 +79,21 @@ public class PerformanceTestMain {
         return entries;
     }
 
-    private static void beadSortTest() {
-        System.out.println("Bead sort:");
+    private static void testSortingAlgorithm(SortingAlgorithm algorithm, String name) {
+        System.out.println(name + " sort:");
         tabCount++;
-        ArrayList<int[]> beadSortInputs = copyInputs(inputs);
+        ArrayList<int[]> inputsCopy = copyInputs(inputs);
         Runtime runtime = Runtime.getRuntime();
         long startTime = System.currentTimeMillis();
         long usedMemoryBefore = runtime.totalMemory() - runtime.freeMemory();
-        for (int[] input : beadSortInputs) {
-            beadSort(input);
+        for (int[] input : inputsCopy) {
+            algorithm.sort(input);
         }
         long time = System.currentTimeMillis() - startTime;
         long usedMemoryAfter = runtime.totalMemory() - runtime.freeMemory();
-        times.put("Bead sort", time);
+        times.put(name, time);
         long memory = (usedMemoryAfter - usedMemoryBefore) / 1000000;
-        memories.put("Bead sort", memory);
-        printTabs();
-        System.out.print("Time: ");
-        System.out.print(time);
-        System.out.println(" ms");
-        printTabs();
-        System.out.print("Memory: ");
-        System.out.print(memory);
-        System.out.println(" MB");
-        tabCount--;
-    }
-
-    private static void bubbleSortTest() {
-        System.out.println("Bubble sort:");
-        tabCount++;
-        ArrayList<int[]> bubbleSortInputs = copyInputs(inputs);
-        Runtime runtime = Runtime.getRuntime();
-        long startTime = System.currentTimeMillis();
-        long usedMemoryBefore = runtime.totalMemory() - runtime.freeMemory();
-        for (int[] input : bubbleSortInputs) {
-            bubbleSort(input);
-        }
-        long time = System.currentTimeMillis() - startTime;
-        long usedMemoryAfter = runtime.totalMemory() - runtime.freeMemory();
-        times.put("Bubble sort", time);
-        long memory = (usedMemoryAfter - usedMemoryBefore) / 1000000;
-        memories.put("Bubble sort", memory);
-        printTabs();
-        System.out.print("Time: ");
-        System.out.print(time);
-        System.out.println(" ms");
-        printTabs();
-        System.out.print("Memory: ");
-        System.out.print(memory);
-        System.out.println(" MB");
-        tabCount--;
-    }
-
-    private static void circleSortTest() {
-        System.out.println("Circle sort:");
-        tabCount++;
-        ArrayList<int[]> circleSortInputs = copyInputs(inputs);
-        Runtime runtime = Runtime.getRuntime();
-        long startTime = System.currentTimeMillis();
-        long usedMemoryBefore = runtime.totalMemory() - runtime.freeMemory();
-        for (int[] input : circleSortInputs) {
-            CircleSort.Sort(input);
-        }
-        long time = System.currentTimeMillis() - startTime;
-        long usedMemoryAfter = runtime.totalMemory() - runtime.freeMemory();
-        times.put("Circle sort", time);
-        long memory = (usedMemoryAfter - usedMemoryBefore) / 1000000;
-        memories.put("Circle sort", memory);
-        printTabs();
-        System.out.print("Time: ");
-        System.out.print(time);
-        System.out.println(" ms");
-        printTabs();
-        System.out.print("Memory: ");
-        System.out.print(memory);
-        System.out.println(" MB");
-        tabCount--;
-    }
-
-    private static void cocktailSortTest() {
-        System.out.println("Cocktail sort:");
-        tabCount++;
-        ArrayList<int[]> cocktailSortInputs = copyInputs(inputs);
-        Runtime runtime = Runtime.getRuntime();
-        long startTime = System.currentTimeMillis();
-        long usedMemoryBefore = runtime.totalMemory() - runtime.freeMemory();
-        for (int[] input : cocktailSortInputs) {
-            new CocktailSort().sort(input);
-        }
-        long time = System.currentTimeMillis() - startTime;
-        long usedMemoryAfter = runtime.totalMemory() - runtime.freeMemory();
-        times.put("Cocktail sort", time);
-        long memory = (usedMemoryAfter - usedMemoryBefore) / 1000000;
-        memories.put("Cocktail sort", memory);
-        printTabs();
-        System.out.print("Time: ");
-        System.out.print(time);
-        System.out.println(" ms");
-        printTabs();
-        System.out.print("Memory: ");
-        System.out.print(memory);
-        System.out.println(" MB");
-        tabCount--;
-    }
-
-    private static void cycleSortTest() {
-        System.out.println("Cycle sort:");
-        tabCount++;
-        ArrayList<int[]> cycleSortInputs = copyInputs(inputs);
-        Runtime runtime = Runtime.getRuntime();
-        long startTime = System.currentTimeMillis();
-        long usedMemoryBefore = runtime.totalMemory() - runtime.freeMemory();
-        for (int[] input : cycleSortInputs) {
-            CycleSort.cycleSort(input, input.length);
-        }
-        long time = System.currentTimeMillis() - startTime;
-        long usedMemoryAfter = runtime.totalMemory() - runtime.freeMemory();
-        times.put("Cycle sort", time);
-        long memory = (usedMemoryAfter - usedMemoryBefore) / 1000000;
-        memories.put("Cycle sort", memory);
-        printTabs();
-        System.out.print("Time: ");
-        System.out.print(time);
-        System.out.println(" ms");
-        printTabs();
-        System.out.print("Memory: ");
-        System.out.print(memory);
-        System.out.println(" MB");
-        tabCount--;
-    }
-
-    private static void gnomeSortTest() {
-        System.out.println("Gnome sort:");
-        tabCount++;
-        ArrayList<int[]> gnomeSortInputs = copyInputs(inputs);
-        Runtime runtime = Runtime.getRuntime();
-        long startTime = System.currentTimeMillis();
-        long usedMemoryBefore = runtime.totalMemory() - runtime.freeMemory();
-        for (int[] input : gnomeSortInputs) {
-            GnomeSort.gnomeSort(input);
-        }
-        long time = System.currentTimeMillis() - startTime;
-        long usedMemoryAfter = runtime.totalMemory() - runtime.freeMemory();
-        times.put("Gnome sort", time);
-        long memory = (usedMemoryAfter - usedMemoryBefore) / 1000000;
-        memories.put("Gnome sort", memory);
-        printTabs();
-        System.out.print("Time: ");
-        System.out.print(time);
-        System.out.println(" ms");
-        printTabs();
-        System.out.print("Memory: ");
-        System.out.print(memory);
-        System.out.println(" MB");
-        tabCount--;
-    }
-
-    private static void heapSortTest() {
-        System.out.println("Heap sort:");
-        tabCount++;
-        ArrayList<int[]> heapSortInputs = copyInputs(inputs);
-        Runtime runtime = Runtime.getRuntime();
-        long startTime = System.currentTimeMillis();
-        long usedMemoryBefore = runtime.totalMemory() - runtime.freeMemory();
-        for (int[] input : heapSortInputs) {
-            HeapSort.sort(input);
-        }
-        long time = System.currentTimeMillis() - startTime;
-        long usedMemoryAfter = runtime.totalMemory() - runtime.freeMemory();
-        times.put("Heap sort", time);
-        long memory = (usedMemoryAfter - usedMemoryBefore) / 1000000;
-        memories.put("Heap sort", memory);
-        printTabs();
-        System.out.print("Time: ");
-        System.out.print(time);
-        System.out.println(" ms");
-        printTabs();
-        System.out.print("Memory: ");
-        System.out.print(memory);
-        System.out.println(" MB");
-        tabCount--;
-    }
-
-    private static void insertionSortTest() {
-        System.out.println("Insertion sort:");
-        tabCount++;
-        ArrayList<int[]> insertionSortInputs = copyInputs(inputs);
-        Runtime runtime = Runtime.getRuntime();
-        long startTime = System.currentTimeMillis();
-        long usedMemoryBefore = runtime.totalMemory() - runtime.freeMemory();
-        for (int[] input : insertionSortInputs) {
-            new InsertionSort().sort(input);
-        }
-        long time = System.currentTimeMillis() - startTime;
-        long usedMemoryAfter = runtime.totalMemory() - runtime.freeMemory();
-        times.put("Insertion sort", time);
-        long memory = (usedMemoryAfter - usedMemoryBefore) / 1000000;
-        memories.put("Insertion sort", memory);
-        printTabs();
-        System.out.print("Time: ");
-        System.out.print(time);
-        System.out.println(" ms");
-        printTabs();
-        System.out.print("Memory: ");
-        System.out.print(memory);
-        System.out.println(" MB");
-        tabCount--;
-    }
-
-    private static void pigeonHoleSortTest() {
-        System.out.println("Pigeon hole sort:");
-        tabCount++;
-        ArrayList<int[]> pigeonHoleSortInputs = copyInputs(inputs);
-        Runtime runtime = Runtime.getRuntime();
-        long startTime = System.currentTimeMillis();
-        long usedMemoryBefore = runtime.totalMemory() - runtime.freeMemory();
-        for (int[] input : pigeonHoleSortInputs) {
-            PigeonHoleSort.pigeonholeSort(input, input.length);
-        }
-        long time = System.currentTimeMillis() - startTime;
-        long usedMemoryAfter = runtime.totalMemory() - runtime.freeMemory();
-        times.put("Pigeon hole sort", time);
-        long memory = (usedMemoryAfter - usedMemoryBefore) / 1000000;
-        memories.put("Pigeon hole sort", memory);
-        printTabs();
-        System.out.print("Time: ");
-        System.out.print(time);
-        System.out.println(" ms");
-        printTabs();
-        System.out.print("Memory: ");
-        System.out.print(memory);
-        System.out.println(" MB");
-        tabCount--;
-    }
-
-    private static void quickSortTest() {
-        System.out.println("Quick sort:");
-        tabCount++;
-        ArrayList<int[]> quickSortInputs = copyInputs(inputs);
-        Runtime runtime = Runtime.getRuntime();
-        long startTime = System.currentTimeMillis();
-        long usedMemoryBefore = runtime.totalMemory() - runtime.freeMemory();
-        for (int[] input : quickSortInputs) {
-            new QuickSort().sort(input, 0, input.length - 1);
-        }
-        long time = System.currentTimeMillis() - startTime;
-        long usedMemoryAfter = runtime.totalMemory() - runtime.freeMemory();
-        times.put("Quick sort", time);
-        long memory = (usedMemoryAfter - usedMemoryBefore) / 1000000;
-        memories.put("Quick sort", memory);
-        printTabs();
-        System.out.print("Time: ");
-        System.out.print(time);
-        System.out.println(" ms");
-        printTabs();
-        System.out.print("Memory: ");
-        System.out.print(memory);
-        System.out.println(" MB");
-        tabCount--;
-    }
-
-    private static void radixSortTest() {
-        System.out.println("Radix sort:");
-        tabCount++;
-        ArrayList<int[]> radixSortInputs = copyInputs(inputs);
-        Runtime runtime = Runtime.getRuntime();
-        long startTime = System.currentTimeMillis();
-        long usedMemoryBefore = runtime.totalMemory() - runtime.freeMemory();
-        for (int[] input : radixSortInputs) {
-            RadixSort.radixSort(input);
-        }
-        long time = System.currentTimeMillis() - startTime;
-        long usedMemoryAfter = runtime.totalMemory() - runtime.freeMemory();
-        times.put("Radix sort", time);
-        long memory = (usedMemoryAfter - usedMemoryBefore) / 1000000;
-        memories.put("Radix sort", memory);
-        printTabs();
-        System.out.print("Time: ");
-        System.out.print(time);
-        System.out.println(" ms");
-        printTabs();
-        System.out.print("Memory: ");
-        System.out.print(memory);
-        System.out.println(" MB");
-        tabCount--;
-    }
-
-    private static void selectionSortTest() {
-        System.out.println("Selection sort:");
-        tabCount++;
-        ArrayList<int[]> selectionSortInputs = copyInputs(inputs);
-        Runtime runtime = Runtime.getRuntime();
-        long startTime = System.currentTimeMillis();
-        long usedMemoryBefore = runtime.totalMemory() - runtime.freeMemory();
-        for (int[] input : selectionSortInputs) {
-            SelectionSort.sort(input);
-        }
-        long time = System.currentTimeMillis() - startTime;
-        long usedMemoryAfter = runtime.totalMemory() - runtime.freeMemory();
-        times.put("Selection sort", time);
-        long memory = (usedMemoryAfter - usedMemoryBefore) / 1000000;
-        memories.put("Selection sort", memory);
-        printTabs();
-        System.out.print("Time: ");
-        System.out.print(time);
-        System.out.println(" ms");
-        printTabs();
-        System.out.print("Memory: ");
-        System.out.print(memory);
-        System.out.println(" MB");
-        tabCount--;
-    }
-
-    private static void shakerSortTest() {
-        System.out.println("Shaker sort:");
-        tabCount++;
-        ArrayList<int[]> shakerSortInputs = copyInputs(inputs);
-        Runtime runtime = Runtime.getRuntime();
-        long startTime = System.currentTimeMillis();
-        long usedMemoryBefore = runtime.totalMemory() - runtime.freeMemory();
-        for (int[] input : shakerSortInputs) {
-            ShakerSort.sort(input);
-        }
-        long time = System.currentTimeMillis() - startTime;
-        long usedMemoryAfter = runtime.totalMemory() - runtime.freeMemory();
-        times.put("Shaker sort", time);
-        long memory = (usedMemoryAfter - usedMemoryBefore) / 1000000;
-        memories.put("Shaker sort", memory);
-        printTabs();
-        System.out.print("Time: ");
-        System.out.print(time);
-        System.out.println(" ms");
-        printTabs();
-        System.out.print("Memory: ");
-        System.out.print(memory);
-        System.out.println(" MB");
-        tabCount--;
-    }
-
-    private static void shellSortTest() {
-        System.out.println("Shell sort:");
-        tabCount++;
-        ArrayList<int[]> shellSortInputs = copyInputs(inputs);
-        Runtime runtime = Runtime.getRuntime();
-        long startTime = System.currentTimeMillis();
-        long usedMemoryBefore = runtime.totalMemory() - runtime.freeMemory();
-        for (int[] input : shellSortInputs) {
-            ShellSort.sort(input);
-        }
-        long time = System.currentTimeMillis() - startTime;
-        long usedMemoryAfter = runtime.totalMemory() - runtime.freeMemory();
-        times.put("Shell sort", time);
-        long memory = (usedMemoryAfter - usedMemoryBefore) / 1000000;
-        memories.put("Shell sort", memory);
+        memories.put(name, memory);
         printTabs();
         System.out.print("Time: ");
         System.out.print(time);
